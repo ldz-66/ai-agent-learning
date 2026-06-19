94abf132bb46bd35351c45c5ef0cd67fd478a237
ldz <3469528426@qq.com>
2026-06-19 11:33:51 +0800

    适配Railway部署，修复502

--- week11/main.py
+++ week11/main.py
@@ -1,6 +1,4 @@
-import os
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -11,7 +9,3 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
