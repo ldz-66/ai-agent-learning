96c2965725dba0c59d41c8d90c852719b95662db
ldz <3469528426@qq.com>
2026-06-19 11:17:56 +0800

    修复fastapi依赖+服务监听0.0.0.0

--- week11/main.py
+++ week11/main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +11,7 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
